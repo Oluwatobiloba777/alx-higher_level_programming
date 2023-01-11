8a8d3dc9993777eba6e3eab10ea7b490e8f61216
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2023-01-11 09:33:49 -0800

    initial commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def sqaure_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
         new_matrix.append(list(map(lambda x: x ** 2, i)))
