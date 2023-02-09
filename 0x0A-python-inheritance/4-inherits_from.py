d8819825104433e1e93d5769fba9e83b3b0e61cb
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2023-02-08 21:11:04 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,5 +10,4 @@ def inherits_from(obj, a_class):
     if isinstance(obj, a_class) and \
         issubclass(a_class, obj.__class__) is False:
         return True
-    else:
-        return False
+    return False
