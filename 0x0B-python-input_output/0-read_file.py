ed915c95ed8aacb0a7fcd9a34032876c581ad8e5
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-02-14 03:32:01 +0100

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 def read_file(filename=""):
     """ function that reads a file """
     with open(filename, encoding="utf-8") as new_file:
-        line = new_file.read()
-        print(line,end="")
+        for line in new_file:
+            print(line, end="")
