b6eada564932e93812ba1db2047851f93181b4b2
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-02-14 03:26:06 +0100

    initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+def read_file(filename=""):
+    """ function that reads a file """
+    with open(filename, encoding="utf-8") as new_file:
+        for line in new_file:
+            print(line,end="")
