9dea31398e4fe6011ba91bf0b47190d17da01014
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2023-02-16 09:40:08 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """  function that reads a file """
-    with open(filename, mode="r", encoding="utf-8") as new_file:
-        for line in new_file:
-            print(line, end="")
+    with open(filename, encoding="utf-8") as new_file:
+        line = new_file.read()
+        print(line, end="")
