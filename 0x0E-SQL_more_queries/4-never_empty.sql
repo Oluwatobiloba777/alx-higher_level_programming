7ac45a2b5dcc3d3222225820ecffe59a6c45b1e4
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2023-03-15 06:06:39 -0700

    initial commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- this script creates the table 'id_not_null' with id column that can't be null
+CREATE TABLE IF NOT EXISTS id_not_null(
+       id INT DEFAULT 1 NOT NULL,
+       name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- this script lists all the cities of California that can be found in the
+-- databse 'hbtn_0d_usa'
+SELECT id, name FROM cities WHERE state_id
+= (SELECT id FROM states WHERE name='California')
+ORDER BY id ASC;
